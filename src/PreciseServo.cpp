f886caf1b2c41113f5ae40822b7a19d8bf0e9463
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-02-15 17:03:36 -0300

    refactor: library restructured and added some comments

--- src/PreciseServo.cpp
+++ src/PreciseServo.cpp
@@ -1,28 +1,29 @@
 #include "PreciseServo.h"
 
-void PreciseServo::_adjustDegValue(i8* deg)
+/** .........................
+ * DServo METHOD DEFFINITIONS
+ */
+
+/** attach the object to the pin and set the min and max deggre values */
+void DServo::config(i8 pin, i8 min=0, i8 max=180)
 {
-    *deg = *deg < _minDeg ? _minDeg : *deg;
-    *deg = *deg > _maxDeg ? _maxDeg : *deg;
+    this->attach(pin);
+    this->write(min); // ... also, it resets to the min value on config
+    this->min = min;
+    this->max = max;
 }
 
-void PreciseServo::config(i8 pin, i8 min = 0, i8 max = 180)
-{
-    this -> attach(pin);
-    this -> write(min); // reset to the min value
-    _minDeg = min;
-    _maxDeg = max;
-}
-
-void PreciseServo::preciseWrite(i8 deg, i16 sleep)
+/** delayed write - sleep x milliseconds each deggre movement to reach the deg position */
+void DServo::dwrite(i8 deg, i8 sleep)
 {
     deg = deg < this->min ? this->min : deg; // use the min value if it is lesser than that
     deg = deg > this->max ? this->max : deg; // use the max value if it is greater than that
 
-    if (!sleep)
-        return this -> write(deg);
+    // it isn't different from a regular write() if the speed is 0
+    if (sleep < 1)
+        return this->write(deg);
 
-    i8 curr = this -> read();
+    i8 curr = this->read();
 
     // while curr != deg, it will approach the target position waiting the speed time each deggre
     for (; curr != deg; deg > curr ? ++curr : --curr)
--- src/PreciseServo.h
+++ src/PreciseServo.h
@@ -1,22 +1,19 @@
-#ifndef BTNHANDLER_H
-#define BTNHANDLER_H
+#ifndef PRECISESERVO_H
+#define PRECISESERVO_H
 
 #include <Arduino.h>
 #include <Servo.h>
 
-#define i8  uint8_t
-#define i16 uint16_t
+#define i8 uint8_t
 
-class PreciseServo: public Servo
+/** delayed servo - class to handle the servo speed with delay() function */
+class DServo: public Servo
 {
-  private:
-    i8 _minDeg;
-    i8 _maxDeg; 
-    void _adjustDegValue(i8*);
-
   public:
-    void config(i8, i8, i8);
-    void preciseWrite(i8, i16);
+    i8 min;
+    i8 max; 
+    void config(i8 pin, i8 min, i8 max);
+    void dwrite(i8 deg, i8 sleep);
 };
 
 #endif
