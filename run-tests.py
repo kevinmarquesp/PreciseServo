189b83244944ffa84829cd4b4ff00b5e7513f3f4
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-02-17 09:40:53 -0300

    feat: python script that integrates with arduinounit library

---
+++ run-tests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import serial
+import os
+
+port = os.getenv('PORT')
+baudrate = os.getenv('BAUDRATE')
+
+
+def controller(line):
+    is_test = 'Test' in line[:4]
+    is_summary = 'Test summary:' in line
+    has_passed = line.split(' ')[-1] == 'passed'
+    exit_code = int('0 failed' not in line)
+
+    if is_summary:
+        print('\n' + line.replace(' test(s).', ''))
+        exit(exit_code)
+
+    elif is_test:
+        color = '\033[32m' if has_passed else '\033[31m'
+        line_words = line.split(' ')
+        line_words[1] = color + line_words[1] + '\033[m'
+        out = ' '.join(line_words) + '\033[m'
+
+    print(out)
+
+
+def main():
+    if None in [port, baudrate]:
+        exit(1)
+
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+
+    while True:
+        line = str(ser.readline(), 'utf-8').replace('\n', '').strip()
+        controller(line)
+
+
+if __name__ == '__main__':
+    main()
