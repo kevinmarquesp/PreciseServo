acf5acae360b380314f0f783692d084e7efa01d6
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-02-19 12:26:56 -0300

    feat(test): different color for the skipped tests

--- run-tests.py
+++ run-tests.py
@@ -11,6 +11,7 @@ def controller(line):
     is_test = 'Test' in line[:4]
     is_summary = 'Test summary:' in line
     has_passed = line.split(' ')[-1] == 'passed'
+    was_skipped = line.split(' ')[-1] == 'skipped'
     exit_code = int('0 failed' not in line)
 
     if is_summary:
@@ -19,6 +20,7 @@ def controller(line):
 
     elif is_test:
         color = '\033[32m' if has_passed else '\033[31m'
+        color = '\033[33m' if was_skipped else color
         line_words = line.split(' ')
         line_words[1] = color + line_words[1] + '\033[m'
         line = ' '.join(line_words) + '\033[m'
