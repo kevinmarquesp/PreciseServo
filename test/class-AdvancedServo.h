e44f005027166bbad4a8812b9e644c18a5dc4d6d
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-02-22 09:57:45 -0300

    test: movement of the advanced servos with while loops

--- test/class-AdvancedServo.h
+++ test/class-AdvancedServo.h
@@ -31,3 +31,49 @@ test(class_AdvancedServo__method__moveUntilDone)
     assertEqual(motor.read(), 180);
     assertEqual(motor.moveId, 1);
 }
+
+test(class_AdvancedServo__method__moveUntilDoneBackwards)
+{
+    AdvancedServo motor;
+    motor.config(P1, 0, 180);
+    motor.write(180);
+
+    assertFalse(motor.moving);
+
+    while (!motor.done)
+    {
+        assertFalse(motor.done);
+        motor.move(0, 10);
+
+        if (motor.done) assertFalse(motor.moving);
+        else assertTrue(motor.moving);
+    }
+
+    assertTrue(motor.done);
+    assertEqual(motor.read(), 0);
+    assertEqual(motor.moveId, 1);
+}
+
+test(class_AdvancedServo__method__moreMovements)
+{
+    AdvancedServo motor;
+
+    const i8 SPEED = 5;
+    motor.config(P1, 0, 180);
+
+    assertFalse(motor.moving);
+
+    while (true)
+    {
+        motor.move(motor.is(0), 180, SPEED)
+            ->move(motor.is(1),   0, SPEED)
+            ->move(motor.is(2), 180, SPEED)
+            ->move(motor.is(3),   0, SPEED);
+
+        if (motor.is(4)) break;
+    }
+
+    assertTrue(motor.done);
+    assertEqual(motor.read(), 0);
+    assertEqual(motor.moveId, 4);
+}
